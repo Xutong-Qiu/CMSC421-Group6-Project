3cdd570a629311a9b974ff4e1caa413904006450
yl2049 <75448846+yl2049@users.noreply.github.com>
2021-04-14 17:57:09 -0400

    Update Parser.py
    changed quantifier cases

--- Parser.py
+++ Parser.py
@@ -1,10 +1,11 @@
 import nltk
 from pattern.en import pluralize, singularize
-# pip install pattern
 
-#run the following once
-#nltk.download('punkt')
-#nltk.download('averaged_perceptron_tagger')
+
+#### run the following once ###
+# pip install pattern
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
 
 verbose = True
 new = True     # set to true when analyzing new syntax
@@ -16,22 +17,26 @@ def parser(sentence):
     tags = nltk.tag.pos_tag(tokens, tagset='universal')   # 'universal' is a simplified tag set
     syntax = [item[1] for item in tags]
 
-    if(syntax == ['NOUN', 'VERB', 'ADJ', '.']):
+    if(syntax == ['NOUN', 'VERB', 'ADJ', '.']):  # Jack is smart.
         modifier = [item[0] for item in tags if item[1] == 'ADJ'][0]
         noun = [item[0] for item in tags if item[1] == 'NOUN'][0]
-        fopl = modifier +'(' + noun + ')'   
-    elif(syntax == ['NOUN', 'VERB', 'ADJ', 'CONJ', 'ADJ', '.']):  # 'CONJ' can be 'and', 'or'
+        fopl = modifier +'(' + noun + ')'
+
+    elif(syntax == ['NOUN', 'VERB', 'ADJ', 'CONJ', 'ADJ', '.']): # Jack is smart and/or kind.
+        # 'CONJ' can be 'and', 'or'
         modifiers = [item for item in tags if item[1] == 'ADJ']
         noun = [item[0] for item in tags if item[1] == 'NOUN'][0]
         conj = [item[0] for item in tags if item[1] == 'CONJ'][0]
         if(conj == 'or'):
-            symbol = ' | '
+            symbol = ' ∨ '
         elif(conj == 'and'):
-            symbol = ' & '
+            symbol = ' ∧ '
         else:
             return('invalid symbol')
-        fopl = modifiers[0][0] +'(' + noun + ')' + symbol + modifiers[1][0] +'(' + noun + ')' 
-    elif(syntax == ['NOUN', 'VERB', 'DET', 'NOUN', '.']):           # 'VERB' is 'is' 
+        fopl = modifiers[0][0] +'(' + noun + ')' + symbol + modifiers[1][0] +'(' + noun + ')'
+
+    elif(syntax == ['NOUN', 'VERB', 'DET', 'NOUN', '.']): # Jack is a student. 
+        # 'VERB' is 'is' 
          nouns = [item for item in tags if item[1] == 'NOUN']
         # noun = [item[0] for item in tags if item[1] == 'NOUN'][0]
          verb = [item[0] for item in tags if item[1] == 'VERB'][0]
@@ -39,7 +44,9 @@ def parser(sentence):
              fopl = nouns[1][0] + '(' + nouns[0][0] + ')'
          else:
              fopl = verb + '(' + nouns[0][0] + ', ' + nouns[1][0] + ')'
-    elif(syntax == ['DET', 'NOUN', 'VERB', 'DET', 'NOUN', '.']):    # VERB is not 'is' (to be or not to be) 
+
+    elif(syntax == ['DET', 'NOUN', 'VERB', 'DET', 'NOUN', '.']): # A dog chases a car.
+        # VERB is not 'is' (to be or not to be)
          verb = [item[0] for item in tags if item[1] == 'VERB'][0]
          nouns = [item for item in tags if item[1] == 'NOUN']
          fopl = verb + '(' + nouns[0][0] + ', ' + nouns[1][0] + ')'
@@ -73,18 +80,30 @@ def parser(sentence):
                 fopl = '¬∃(X) ((' + nouns[0] + '(X) → (' + verb + '(X, ' + nouns[1]+ ')))'
 
 
-    
-    
-#     elif(syntax == ['DET', 'NOUN', 'VERB', 'ADJ', '.'] and tokens[0] == 'All' and tokens[2] == 'are'):
-#         fopl = 'All(x) ' + tokens[3] + '(' + tokens[1] + ')' 
-#     elif(syntax == ['DET', 'NOUN', 'VERB', 'ADJ', '.'] and tokens[0] == 'Some' and tokens[2] == 'are'):
-#         fopl = 'Ex(x) ' + tokens[3] + '(' + tokens[1] + ')' 
-#     elif(syntax == ['NOUN', 'VERB', 'DET', 'ADJ', 'NOUN', '.']):    # 'VERB' is 'is'  -- need to distinguish not 'is'
-#          modifier = [item[0] for item in tags if item[1] == 'ADJ'][0]
-#          nouns = [item for item in tags if item[1] == 'NOUN']
-#          fopl = modifier + '(' + nouns[0][0] + ') & ' + nouns[1][0] + '(' + nouns[0][0] + ')'
+
+    elif(syntax == ['DET', 'NOUN', 'VERB', 'ADJ', '.'] and tokens[0] == 'All' and 
+    (tokens[2] == 'are' or tokens[2] == 'is')):
+        # All water is precious.        All dogs are nice.
+        # fopl = 'All(x) ' + tokens[3] + '(' + tokens[1] + ')' 
+        nouns = [singularize(item[0]) for item in tags if item[1] == 'NOUN']
+        fopl = '∀(X) ((' + nouns[0] + '(X) → ' + tokens[3] + '(X' + '))' 
+
+    elif(syntax == ['DET', 'NOUN', 'VERB', 'ADJ', '.'] and tokens[0] == 'Some' and 
+    (tokens[2] == 'are' or tokens[2] == 'is')):
+        # Some water is expensive.      Some cats are nice.
+        # fopl = '∃(x) ' + tokens[3] + '(' + tokens[1] + ')' 
+        nouns = [singularize(item[0]) for item in tags if item[1] == 'NOUN']
+        fopl = '∃(X) ((' + nouns[0] + '(X) → ' + tokens[3] + '(X' + '))' 
+
+    elif(syntax == ['NOUN', 'VERB', 'DET', 'ADJ', 'NOUN', '.']): # Jack is a smart student.
+        # 'VERB' is 'is' -- need to distinguish not 'is'
+         modifier = [item[0] for item in tags if item[1] == 'ADJ'][0]
+         nouns = [item for item in tags if item[1] == 'NOUN']
+         fopl = modifier + '(' + nouns[0][0] + ') & ' + nouns[1][0] + '(' + nouns[0][0] + ')'
+
     elif(new):  
         fopl = 'undefined'    
+
     else:
         return ('Syntax not recognized: ', syntax)
         
@@ -106,7 +125,6 @@ parser('Socrates is a philospher.')
 parser('A dog chases a car.')
 parser('Socrates is a mortal philospher.')
 parser('Joe climbs a ladder.')
-parser('Joe climbs a ladder.')
 
 print('***Multiple sentences***\n')
 text = 'A dog chases a car. Socrates is a mortal philospher. '
@@ -116,7 +134,9 @@ for sentence in sentences:
     
 print("***Quantifiers***")
 parser('Some cats are nice.')
-
+parser('All dogs are nice.')
+parser('All water is precious.')
+parser('Some water is expensive.')
 parser('All men are mortals.')
 parser('No cats loves dog.')
 # parser('A cat loves fish.') # ['DET', 'NOUN', 'VERB', 'ADJ', '.'], nlkt library error.
