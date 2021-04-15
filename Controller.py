30578eb9e7ad9a3bf437d234fb1e664c57fe4fe0
Xutong <64801351+Xutong-Qiu@users.noreply.github.com>
2021-04-15 09:06:02 -0400

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,5 +1,6 @@
 from coreClasses import *
 from Solver import Solver
+import Parser
 
 print('Command: add knowledge, solve, quit')
 s=Solver()
@@ -8,7 +9,7 @@ while True:
     if command == 'add knowledge':
         knowledge = input("Enter the knowledge:")
         while knowledge != 'done':
-            foplstring=parser(knowledge)
+            foplstring=Parser.parser(knowledge)
             fopl=converter1(foplstring)
             clause=converter2(fopl)
             s.add_knowledge(clause)
