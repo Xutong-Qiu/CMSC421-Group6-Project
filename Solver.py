b2fbd5db64fdb734a029d8d9c48e9a6a68bb9d48
Xutong <64801351+Xutong-Qiu@users.noreply.github.com>
2021-04-17 10:55:36 -0400

    Add files via upload

--- Solver.py
+++ Solver.py
@@ -4,12 +4,16 @@ import numpy as np
 
 class Solver:
     #clauses: an array of clauses
-    def __init__(self, clauses=np.array([])):
-        self.kb = clauses #knowledge base
-
+    def __init__(self):
+        self.kb = np.array([]) #knowledge base
+        self.uniPre = np.array([])
+        
     def add_knowledge(self, new_k):
         if not self.contain_knowledge(new_k):
             self.kb = np.append(self.kb,new_k)
+        for predicate in new_k.predicates:
+            if not predicate in self.uniPre:
+                self.uniPre = np.append(self.uniPre, predicate)
 
     def contain_knowledge(self, k):
         for i in self.kb:
@@ -17,18 +21,26 @@ class Solver:
                 return True
         return False
 
+    def solvable(self, conjecture):
+        for predicate in conjecture.predicates:
+            if not predicate in self.uniPre:
+                return False
+        return True
+
+
     def res(self):
-        for i in range(0, len(self.kb)):
-            for j in range(i+1, len(self.kb)):
+        kbcopy = np.array(self.kb)
+        for i in range(0, len(kbcopy)):
+            for j in range(i+1, len(kbcopy)):
                 if i == j:
                     continue
-                result = self.kb[i].resolution(self.kb[j])
-                print('result: {} |{} -> {}'.format(self.kb[i],self.kb[j],result))
+                result = kbcopy[i].resolution(kbcopy[j])
+                print('result: {} |{} -> {}'.format(kbcopy[i],kbcopy[j],result))
                 if  result != None:
                     if result.is_empty():
                        return True
                     else:
-                        self.add_knowledge(result)
+                        kbcopy = np.append(kbcopy, result)
         return False
                 
     
