f7a4c13c92d839ef1d2553325eede14de7f57f8f
Xutong <64801351+Xutong-Qiu@users.noreply.github.com>
2021-04-20 11:45:49 -0400

    Add files via upload

--- Solver.py
+++ Solver.py
@@ -6,14 +6,14 @@ class Solver:
     #clauses: an array of clauses
     def __init__(self):
         self.kb = np.array([]) #knowledge base
-        self.uniPre = np.array([])
+        self.uniPreName = np.array([])
         
     def add_knowledge(self, new_k):
         if not self.contain_knowledge(new_k):
             self.kb = np.append(self.kb,new_k)
         for predicate in new_k.predicates:
-            if not predicate in self.uniPre:
-                self.uniPre = np.append(self.uniPre, predicate)
+            if not self.contain_predicate(predicate):
+                self.uniPreName = np.append(self.uniPreName, predicate.name)
 
     def contain_knowledge(self, k):
         for i in self.kb:
@@ -21,11 +21,47 @@ class Solver:
                 return True
         return False
 
-    def solvable(self, conjecture):
-        for predicate in conjecture.predicates:
-            if not predicate in self.uniPre:
-                return False
+    def contain_predicate(self, predicate):
+        for i in self.uniPreName:
+                if i == predicate.name:
+                    return True
+        return False
+    
+    def solvable(self, conjclauses):
+        for clauses in conjclauses:
+            for predicate in clauses.predicates:
+                if not self.contain_predicate(predicate):
+                    print(predicate)
+                    return False
         return True
+
+    def solve(self, conjclauses):
+        if not self.solvable(conjclauses):
+            return 'not enough info'
+        else:
+            support = conjclauses
+            kbcopy = np.array(self.kb)
+            answer = None
+            lengthsupport=len(support)-1
+            while lengthsupport != len(support):
+                print(len(support),lengthsupport)
+                lengthsupport=len(support)
+                for i in range(0, len(support)):
+                    for j in range(0, len(kbcopy)):
+                        result = support[i].resolution(kbcopy[j])
+                        print('result: {} | {} -> {}'.format(support[i],kbcopy[j],result))
+                        if  result != None:
+                            if result.is_empty():
+                                return 'True'
+                            else:#check if knowledge exists, if not, add
+                                exist = False
+                                for i in support:
+                                    if result == i:
+                                        #print('equal')
+                                        exist = True
+                                if not exist:
+                                    support = np.append(support, result)
+            return 'False'
 
 
     def res(self):
--- debug.py
+++ debug.py
@@ -1,9 +1,42 @@
 from coreClasses import *
 from Solver import Solver
+from Parser import parser
+import nltk
+'''
+parser('Socrates is mortal.')
+parser('Socrates is mortal and Greek.')
+parser('Socrates is mortal or Greek.')
+parser('Socrates is a philospher.')
+parser('A dog chases a car.')
+parser('Socrates is a mortal philospher.')
+parser('Joe climbs a ladder.')
+parser('Joe climbs a ladder.')
+
+print('***Multiple sentences***\n')
+text = 'A dog chases a car. Socrates is a mortal philospher. '
+sentences = nltk.sent_tokenize(text)
+for sentence in sentences:
+    parser(sentence)
+    
+print("***Quantifiers***")
+parser('Some cats are nice.')
+
+parser('All men are mortals.')
+parser('No cats loves dog.')
+# parser('A cat loves fish.') # ['DET', 'NOUN', 'VERB', 'ADJ', '.'], nlkt library error.
+parser('Some cats catch mice.')
+'''
+# syllogism
+#text = 'All people are mortal. Socrates is a person. Therefore, Socrates is mortal.'
+#sentences = nltk.sent_tokenize(text)
+#for sentence in sentences:
+#   parser(sentence)
 
 
 #debug code
 a = Predicate("likes", 'X', VAR)#likes(X)
+nota=Predicate("likes", 'X', VAR)
+a.negate()
 #a.negate()#~likes(X)
 b = Predicate("likes", 'a', CONST)#likes(a)
 c = Predicate("food", 'b', CONST)#food(b)
@@ -11,17 +44,21 @@ d=Predicate("food", 'b', CONST)#food(b)
 c.negate()#~food(b)
 clause1=Clause(np.array([a,c]))#likes(X)V~food(b)
 clause2=Clause(np.array([b,d]))#likes(a)Vfood(b)
-#clause1=Clause(np.array([c]))#likes(X)V~food(b)
-#clause2=Clause(np.array([d]))#likes(a)Vfood(b)
+clause3=Clause(np.array([a]))
+clause4=Clause(np.array([nota]))
 #print(clause1)
 #print(clause2)
-print(clause1.resolution(clause2))
+print(clause3)
+print(clause4)
 s = Solver()
-s.add_knowledge(clause1)
+s.add_knowledge(clause3)
+#print('is solvable? {}'.format(s.solvable([clause2])))
+#print(clause1.resolution(clause2)==clause2.resolution(clause1))
+print(s.solve([clause4]))
 s.add_knowledge(clause2)
-print(s.res())
-'''#print(a.negation)
-
+#print(s.res())
+#print(a.negation)
+'''
 #s=Solver()
 test3=Clause(np.array([a]))
 test4=Clause(np.array([b]))
@@ -30,16 +67,16 @@ sub=a.unifiable(b)
 d.substitution(sub)
 print(d)
 print(a.unifiable(b))
-'''
+
 #test FOPL
 x = FOPL(a, None, None)
 y=FOPL(ALL, x, 'X')
 y.negate()
 #y.negate()
 #print(y)
+'''
 
-
-
+'''
 ab3 = Clause([Predicate("ab", 'b', CONST, 'c', CONST)])#ab(b,c)
 ab2 = Clause([Predicate("ab", 'a', CONST, 'b', CONST)])#ab(a,b)
 in1=Clause([Predicate("in", 'e', CONST, 'f', CONST)])#in(e,f)
@@ -54,3 +91,6 @@ c6=Clause([in6,rt6,rt62])
 
 rt7 = Predicate("rt", 'X1', VAR, 'Y1', VAR)#rt(X1,Y1)
 rt7.negate()
+for i in s.uniPre:
+    print(i)
+'''
